d4fff52e52dab89330c5e2bbbaa457ea230316a2
krishnareddy756 <saikrishnareddykalvapalli00@gmail.com>
2025-10-21 17:26:01 +0530

    Created Page Objects for WebPages

---
+++ src/main/java/com/saucedemo/pages/LoginPage.java
@@ -0,0 +1,28 @@
+package com.saucedemo.pages;
+
+import org.openqa.selenium.By;
+
+public class LoginPage extends BasePage {
+    private By usernameFeild=By.id("user-name");
+    private By passwordFeild=By.id("password");
+    private By loginButtonFeild=By.id("login-button");
+    private By errorMessage = By.xpath("#login_button_container h3");
+    public void setUsername(String username){
+        set(usernameFeild,username);
+    }
+    public void setPassword(String password){
+        set(passwordFeild,password);
+    }
+    public ProductsPage clickLoginButton(){
+        click(loginButtonFeild);
+        return new ProductsPage();
+    }
+    public ProductsPage loginIntoApplication(String username,String password){
+        setUsername(username);
+        setPassword(password);
+        return clickLoginButton();
+    }
+    public String getErrorMessage(){
+        return find(errorMessage).getText();
+    }
+}
